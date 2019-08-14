5eea46ad5f22a2ab20a1feff5419536d55468681
ishu2307 <53898406+ishu2307@users.noreply.github.com>
2019-08-14 17:09:54 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n%2==0)
+{
+	printf("even");
+}
+else if(n%2!=0)
+{
+	printf("odd");
+	
+		
+	}
+	else
+	{
+	printf("invalid")
+}
+return 0;
+}
+
