049dcd2fb330c4db35b7d95597ba6b3cfa5392a4
ishu2307 <53898406+ishu2307@users.noreply.github.com>
2019-08-17 15:36:32 +0530

    Create eveninterval.c

---
+++ eveninterval.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+
+int main(){
+
+           int a,b,rem,i;
+           scanf("%d",a);
+
+           scanf("%d",b);
+
+           scanf("%d%d",a,b); 
+
+           for(i=a; i<=b; ++i){
+
+               rem = i % 2;
+
+               if(rem == 0)
+
+               printf("\n  %d",i);
+
+           }
+
+    return 0; 
+
+    } 
